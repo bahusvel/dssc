dd71362cf3aa53daa13d60adcc05ddec6dc5ff86
Denis Lavrov <bahus.vel@gmail.com>
2017-12-08 11:50:03 +1300

    Better handlind of eof

--- src/linedssc/main.rs
+++ src/linedssc/main.rs
@@ -9,16 +9,23 @@ use byteorder::{BigEndian, WriteBytesExt, ReadBytesExt};
 fn main() {
     if let Some(d) = env::args().nth(1) {
         if d != "-d".to_string() {
-            println!("Usage: linedssc [-d] (d means decompress)");
+            eprintln!("Usage: linedssc [-d] (d means decompress)");
             return;
         }
         let mut decoder = DSSCDecoder::new();
         loop {
             let len = stdin().read_u32::<BigEndian>().expect("Unexpected input");
             let mut buf = Vec::new();
-            if let Err(error) = stdin().take(len as u64).read_to_end(&mut buf) {
-                println!("error: {}", error);
-                return;
+            match stdin().take(len as u64).read_to_end(&mut buf) {
+                Err(error) => {
+                    eprintln!("error: {}", error);
+                    return;
+                }
+                Ok(n) => {
+                    if n == 0 {
+                        return;
+                    }
+                }
             }
             stdout().write_all(&decoder.decode(&buf)).expect(
                 "write failed",
@@ -28,9 +35,16 @@ fn main() {
     let mut encoder = DSSCEncoder::new();
     loop {
         let mut input = String::new();
-        if let Err(error) = stdin().read_line(&mut input) {
-            println!("error: {}", error);
-            return;
+        match stdin().read_line(&mut input) {
+            Err(error) => {
+                eprintln!("error: {}", error);
+                return;
+            }
+            Ok(n) => {
+                if n == 0 {
+                    return;
+                }
+            }
         }
         let encoded = encoder.encode(input.as_bytes());
         stdout()
