98bd9666077c4a250c9c5948bf5bc017b5847318
Denis Lavrov <bahus.vel@gmail.com>
2017-12-08 11:33:17 +1300

    Added test and some fixes

--- src/varint.rs
+++ src/varint.rs
@@ -11,19 +11,29 @@ pub fn put_uvarint(buf: &mut [u8], mut x: u64) -> usize {
     return i + 1;
 }
 
-pub fn uvarint(buf: &[u8]) -> (u64, usize) {
+pub fn uvarint(buf: &[u8]) -> (u64, isize) {
     let mut x = 0u64;
-    let mut s = 0usize;
+    let mut s = 0isize;
     for i in 0..buf.len() {
         let b = buf[i];
         if b < 0x80 {
             if i > 9 || i == 9 && b > 1 {
-                return (0u64, <usize>::max_value() - (i + 1)); // overflow
+                return (0u64, -1); // overflow
             }
-            return (x | (b as u64) << s, (i + 1) as usize);
+            return (x | (b as u64) << s, (i + 1) as isize);
         }
         x |= ((b & 0x7f) as u64) << s;
         s += 7;
     }
-    (0u64, 0usize)
+    (0u64, 0isize)
+}
+
+#[test]
+pub fn varint_test() {
+    let mut buf = [0; 9];
+    let val = 10000;
+    println!("{}", put_uvarint(&mut buf, val));
+    let (after, size) = uvarint(&buf);
+    println!("{} {}", after, size);
+    assert_eq!(val, after);
 }
