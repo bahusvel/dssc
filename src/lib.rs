2f9f920bf54997ddde2666c17df61b5852dbc217
Denis Lavrov <denislavrov@Deniss-MacBook-Pro-5.local>
2017-12-09 13:07:11 +1300

    Chuncked compressor is done

--- src/lib.rs
+++ src/lib.rs
@@ -206,72 +206,124 @@ fn chunk_match(needle: &[u8], haystacks: &Vec<Vec<u8>>) -> Vec<Vec<usize>> {
     return results;
 }
 
+#[derive(Debug, PartialEq)]
+enum BlockType {
+    Delta,
+    Original,
+}
+
 #[derive(Debug)]
-enum Block {
-    Delta { offset: usize, len: usize },
-    Original { needle_off: usize, len: usize },
+struct Block {
+    block_type: BlockType,
+    needle_off: usize,
+    offset: usize,
+    len: usize,
 }
 
 impl Block {
-    fn fit(&mut self, needle: &[u8], needle_off: usize, haystack: &Vec<u8>) {
-        if let &mut Block::Delta {
-            offset: ref mut offset,
-            len: ref mut len,
-        } = self
+    fn fit(&mut self, needle: &[u8], haystack: &Vec<u8>) {
+        if self.block_type != BlockType::Delta {
+            return;
+        }
+        let mut bi = 0;
+        let mut od = 0;
+        let mut nod = 0;
+        while (self.needle_off - bi) > 0 && (self.offset - bi) > 0 &&
+            needle[self.needle_off - bi] == haystack[self.offset - bi]
         {
-            let mut bi = 0;
-            let mut od = 0;
-            while (needle_off - bi) > 0 && (*offset - bi) > 0 &&
-                needle[needle_off - bi] == haystack[*offset - bi]
-            {
-                od += 1;
-                *len += 1;
-                bi += 1;
+            od += 1;
+            nod += 1;
+            self.len += 1;
+            bi += 1;
+        }
+        let mut fi = self.len;
+        while (self.needle_off + fi) < needle.len() && (self.offset + fi) < haystack.len() &&
+            needle[self.needle_off + fi] == haystack[self.offset + fi]
+        {
+            self.len += 1;
+            fi += 1;
+        }
+        self.offset -= od;
+        self.needle_off -= nod;
+    }
+
+    fn encode(&self, needle: &[u8], buf: &mut Vec<u8>) {
+        let mut varint_buf = [0; 10];
+        match self.block_type {
+            BlockType::Delta => {
+                let offset_len = put_uvarint(&mut varint_buf, (self.offset + 1) as u64);
+                buf.extend_from_slice(&varint_buf[0..offset_len]);
+                let len_len = put_uvarint(&mut varint_buf, (self.len) as u64);
+                buf.extend_from_slice(&varint_buf[0..len_len]);
             }
-            let mut fi = *len;
-            println!(
-                "{:?}\n{:?}",
-                from_utf8(&needle[needle_off..]),
-                from_utf8(&haystack[*offset..])
-            );
-            while (needle_off + fi) < needle.len() && (*offset + fi) < haystack.len() &&
-                needle[needle_off + fi] == haystack[*offset + fi]
-            {
-                *len += 1;
-                fi += 1;
+            BlockType::Original => {
+                buf.push(0 as u8);
+                let len_len = put_uvarint(&mut varint_buf, (self.len) as u64);
+                buf.extend_from_slice(&varint_buf[0..len_len]);
+                buf.extend_from_slice(&needle[self.needle_off..self.needle_off + self.len])
             }
-            *offset -= od;
         }
     }
 }
 
 
 fn expand_blocks(needle: &[u8], haystack: &Vec<u8>, result: &Vec<usize>) -> Vec<Block> {
-    let mut vec = Vec::new();
+    let mut blocks = Vec::new();
     let mut ri = 0;
     while ri < result.len() {
         if result[ri] != 0 {
             let bi = ri;
-            let (mut offset, mut len) = (result[ri] - 1, 4);
+            let (offset, mut len) = (result[ri] - 1, 4);
             ri += 1;
             while ri < result.len() && result[ri] != 0 {
                 len += 4;
                 ri += 1;
             }
-            let mut block = Block::Delta {
+            let mut block = Block {
+                block_type: BlockType::Delta,
+                needle_off: bi * 4,
                 offset: offset,
                 len: len,
             };
-            block.fit(needle, bi * 4, haystack);
-            vec.push(block);
-            continue;
+            block.fit(needle, haystack);
+            blocks.push(block);
+        } else {
+            let block = if let Some(last_block) = blocks.last() {
+                Block {
+                    block_type: BlockType::Original,
+                    needle_off: last_block.needle_off + last_block.len,
+                    offset: 0,
+                    len: 0,
+                }
+            } else {
+                Block {
+                    block_type: BlockType::Original,
+                    needle_off: 0,
+                    offset: 0,
+                    len: 0,
+                }
+            };
+            blocks.push(block);
+            while ri < result.len() && result[ri] == 0 {
+                ri += 1;
+            }
         }
-        ri += 1;
     }
-    vec
+    for i in 0..blocks.len() - 1 {
+        if blocks[i].block_type == BlockType::Original {
+            blocks[i].len = blocks[i + 1].needle_off - blocks[i].needle_off;
+        }
+    }
+    if let Some(ref mut last_block) = blocks.last_mut() {
+        if last_block.block_type == BlockType::Original {
+            last_block.len = (needle.len() - 1) - last_block.needle_off; // I am not sure about that -1
+        }
+    }
+    blocks.retain(|b| b.len != 0);
+    blocks
 }
 
-fn chunk_compressor(needle: &[u8], haystacks: &Vec<Vec<u8>>) {
+fn chunk_compressor(needle: &[u8], haystacks: &Vec<Vec<u8>>) -> Vec<u8> {
     let matches = chunk_match(needle, haystacks);
     println!("matches {:?}", matches);
     let max: usize = matches
@@ -279,15 +331,28 @@ fn chunk_compressor(needle: &[u8], haystacks: &Vec<Vec<u8>>) {
         .map(|m| m.iter().filter(|&&o| o != 0).count())
         .max()
         .expect("haystacks are empty");
+    let mut max_block = (0, 0, None);
     for (hi, result) in matches.iter().enumerate() {
         if result.iter().filter(|&&o| o != 0).count() != max {
             continue;
         }
-        println!("{}:{:?}", hi, expand_blocks(needle, &haystacks[hi], result));
-
+        let blocks = expand_blocks(needle, &haystacks[hi], result);
+        let score = blocks
+            .iter()
+            .filter(|b| b.block_type == BlockType::Delta)
+            .map(|b| b.len)
+            .sum();
+        if max_block.0 <= score {
+            max_block = (score, hi, Some(blocks))
+        }
     }
-
-    //println!("max matches {:?}", max_matches);
+    let mut buf = vec![max_block.1 as u8];
+    for block in max_block.2.expect("No candidate was found") {
+        block.encode(needle, &mut buf);
+    }
+    println!("{:?}", needle);
+    println!("{:?}", buf);
+    buf
 }
 
 #[test]
